d1744196f4b0931240869085eeccdf9ae481a832
prehistoriccraftman <165899138+prehistoriccraftman@users.noreply.github.com>
2024-04-18 12:01:56 +0200

    remove useless files

--- UserSaves/Textures-16.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Textures-16" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../../../../Ressources/Textures/Textures-16.png" trans="898989" width="512" height="512"/>
-</tileset>
